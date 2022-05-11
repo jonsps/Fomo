4dc0151537ce604509e65f7214edfd7666c51330
jonsps <jonpool94@gmail.com>
2022-05-10 22:47:49 -0400

    Actualizacion 0.1

--- fomo.js
+++ fomo.js
@@ -1,1 +1,8 @@
 
+    var resultado = Math.round(Math.random()*(max - min) + parseInt(min));
+    var fomonumber = document.querySelector("#fomonumber");
+    var fomociudad = document.querySelector("#fomociudad");
+    
+    fetch("https://ipwhois.app/json/")
+    .then((response) => response.json())
+    .then((data) => (fomonumber.innerHTML = resultado; fomonumber.innerHTML = data.city ; ))
